fc59b097efd0699b4ea0975c1e16c52f1ade2653
DaviVeigaJardim <davijardimrp@gmail.com>
2023-11-30 01:27:08 -0300

    Adicionando função ao botão de reprodução aleatória

--- interfaceGráfica.py
+++ interfaceGráfica.py
@@ -1,6 +1,6 @@
 from tkinter import *
 from tkinter import ttk
-
+import random
 class Aplicacao:
     def __init__(self, master):
         self.raiz = master
@@ -11,51 +11,76 @@ class Aplicacao:
         self.raiz.mainloop()
 
     def quadros_da_tela(self):
-        self.frame_1 = Frame(self.raiz, bd=4, bg='#DDA0DD',
-                             highlightbackground='#D8BFD8', highlightthickness=3)
+        self.frame_1 = Frame(self.raiz, bd=0, bg='#1C1C1C',
+                             highlightbackground='#1C1C1C', highlightthickness=3)
         self.frame_1.place(relx=0.02, rely=0.02, relwidth=0.96, relheight=0.15)
 
-        self.frame_2 = Frame(self.raiz, bd=4, bg='#DDA0DD',
-                             highlightbackground='#D8BFD8', highlightthickness=3)
+        self.frame_2 = Frame(self.raiz, bd=0, bg='#1C1C1C',
+                             highlightbackground='#1C1C1C', highlightthickness=3)
         self.frame_2.place(relx=0.02, rely=0.2, relwidth=0.96, relheight=0.76)
 
     def configurando_janela(self):
-        self.raiz.configure(background='#DA70D6')
+        self.raiz.configure(background='#1C1C1C')
         self.raiz.title("Contador de histórias")
         self.raiz.geometry('600x600')
         self.raiz.resizable(True, True)
 
     def componentes(self):
-        self.botao_reproduzir_clicado = False  
 
-        self.bt_play = Button(self.frame_1, text='PLAY\ \nPAUSE', bd=3, bg='#DA70D6', fg='white', font=('verdana', 10, 'bold'), command=self.botao_reproduzir_clicado)
+        self.bt_play = Button(self.frame_1, text='STOP', bd=0, bg='#FF8C00', fg='black', font=('verdana', 10, 'bold'), command=self.funcao_parar)
         self.bt_play.place(relx=0.01, rely=0.1, relheight=0.6, relwidth=0.2)
 
-        self.bt_aleatorio = Button(self.frame_2, text=' reprodução \n aleatoria', bd=2, bg='#DA70D6', fg='white', font=('verdana', 10, 'bold'))
+        self.bt_aleatorio = Button(self.frame_2, text=' reprodução \n aleatoria', bd=0, bg='#FF8C00', fg='black', font=('verdana', 10, 'bold'), command = self.reproducao_aleatoria)
         self.bt_aleatorio.place(relx=0.01, rely=0.1, relheight=0.10, relwidth=0.2)
 
-        self.bt_sequencial = Button(self.frame_2, text='reprodução \n sequencial', bd=2, bg='#DA70D6', fg='white', font=('verdana', 10, 'bold'))
+        self.bt_sequencial = Button(self.frame_2, text='reprodução \n sequencial', bd=0, bg='#FF8C00', fg='black', font=('verdana', 10, 'bold'))
         self.bt_sequencial.place(relx=0.01, rely=0.3, relheight=0.10, relwidth=0.2)
 
-        self.label_mensagem = Label(self.frame_2, text='', font=('verdana', 12, 'bold'), bg='#DDA0DD')
-        self.label_mensagem.place(relx=0.25, rely=0.5)
+        self.label_mensagem = Label(self.frame_2, text='', font=('verdana', 12, 'bold'), bg='#1C1C1C', fg = '#FF8C00')
+        self.label_mensagem.place(relx=0.4, rely=0.3)
+
+        self.label_mensagem_mes = Label(self.frame_2, text='', font = ('verdana', 12, 'bold' ), bg ='#1C1C1C', fg ='#FF8C00')
+        self.label_mensagem_mes.place(relx = 0.7, rely = 0.3)
+
+    def funcao_parar(self):
+        self.botao_reproduzir_clicado = False
+        self.label_mensagem.config(text= 'Reprodução parada')
 
     def combobox(self):
-        valores_dias = list(range(1, 31))
-        self.combo_dias = ttk.Combobox(self.frame_1, values=valores_dias)
+        self.valores_dias = list(range(1, 31))
+        self.combo_dias = ttk.Combobox(self.frame_1, values=self.valores_dias)
         self.combo_dias.set('Dia')
-        self.combo_dias.place(relx=0.3, rely=0.1, relheight=0.6, relwidth=0.1)
+        self.combo_dias.place(relx=0.3, rely=0.2, relheight=0.4, relwidth=0.2)
+        self.combo_dias.bind("<<ComboboxSelected>>", self.selecionar_dia)
 
-        valores_mes = list(range(1, 13))
-        self.combo_meses = ttk.Combobox(self.frame_1, values=valores_mes)
+        self.valores_mes = ['Janeiro', 'Fevereiro','março','abril','maio','junho','julho','agosto','setembro','outubro', 'novembro', 'dezembro']
+        self.combo_meses = ttk.Combobox(self.frame_1, values=self.valores_mes)
         self.combo_meses.set('Mês')
-        self.combo_meses.place(relx=0.5, rely=0.1, relheight=0.6, relwidth=0.1)
+        self.combo_meses.place(relx=0.6, rely=0.2, relheight=0.4, relwidth=0.2)
+        self.combo_meses.bind("<<ComboboxSelected>>", self.selecionar_mes)
 
-    def reproduzir_historia(self):
-        if not self.botao_reproduzir_clicado:
-            self.label_mensagem.config(text='Reproduzindo história...')
-            self.label_mensagem.place(relx = 0.4, rely = 0.3)
-            self.play_clicked = True
+    def selecionar_dia(self, event):
+        self.atualizar_label_mensagem()
+
+    def selecionar_mes(self, event):
+        self.atualizar_label_mensagem()
+
+    def atualizar_label_mensagem(self):   
+        dia_selecionado = self.combo_dias.get()
+        mes_selecionado = self.combo_meses.get()
+
+        if dia_selecionado != 'Dia' and mes_selecionado != 'Mês':
+            mensagem = (f'Historia do dia {dia_selecionado} de {mes_selecionado}')
+            self.label_mensagem.config(text=mensagem)
+        else:
+            self.label_mensagem.config(text = '')
+            self.label_mensagem_mes.config(text= '')
+    def reproducao_aleatoria(self):
+        self.botao_aleatorio_clicado = False
+        self.combo_dias.set(random.randint(0,31))
+        self.combo_meses.set(random.choice(self.valores_mes))
+        self.atualizar_label_mensagem()
+        
 
 
 raiz = Tk()
