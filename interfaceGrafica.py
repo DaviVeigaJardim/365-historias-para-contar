ec23c737a19c3379cc7befdb03b79264baa56694
DaviVeigaJardim <davijardimrp@gmail.com>
2023-11-30 20:05:27 -0300

    Corrigindo nome para quadros e retirando acento do arquivo

--- interfaceGrafica.py
+++ interfaceGrafica.py
@@ -11,13 +11,13 @@ class Aplicacao:
         self.raiz.mainloop()
 
     def quadros_da_tela(self):
-        self.frame_1 = Frame(self.raiz, bd=0, bg='#1C1C1C',
-                             highlightbackground='#1C1C1C', highlightthickness=3)
-        self.frame_1.place(relx=0.02, rely=0.02, relwidth=0.96, relheight=0.15)
+        self.quadro_1 = Frame(self.raiz, bd=0, bg='#1C1C1C',
+                             highlightbackground='#1C1C1C', highlightthickness=1)
+        self.quadro_1.place(relx=0.02, rely=0.02, relwidth=0.96, relheight=0.15)
 
-        self.frame_2 = Frame(self.raiz, bd=0, bg='#1C1C1C',
-                             highlightbackground='#1C1C1C', highlightthickness=3)
-        self.frame_2.place(relx=0.02, rely=0.2, relwidth=0.96, relheight=0.76)
+        self.quadro_2 = Frame(self.raiz, bd=0, bg='#1C1C1C',
+                             highlightbackground='#363636', highlightthickness=2)
+        self.quadro_2.place(relx=0.02, rely=0.2, relwidth=0.96, relheight=0.76)
 
     def configurando_janela(self):
         self.raiz.configure(background='#1C1C1C')
@@ -27,19 +27,19 @@ class Aplicacao:
 
     def componentes(self):
 
-        self.bt_play = Button(self.frame_1, text='STOP', bd=0, bg='#FF8C00', fg='black', font=('verdana', 10, 'bold'), command=self.funcao_parar)
+        self.bt_play = Button(self.quadro_1, text='STOP', bd=5, bg='#FF8C00', fg='black', font=('verdana', 10, 'bold'), command=self.funcao_parar)
         self.bt_play.place(relx=0.01, rely=0.1, relheight=0.6, relwidth=0.2)
 
-        self.bt_aleatorio = Button(self.frame_2, text=' reprodução \n aleatoria', bd=0, bg='#FF8C00', fg='black', font=('verdana', 10, 'bold'), command = self.reproducao_aleatoria)
+        self.bt_aleatorio = Button(self.quadro_2, text=' reprodução \n aleatoria', bd=5, bg='#FF8C00', fg='black', font=('verdana', 10, 'bold'), command = self.reproducao_aleatoria)
         self.bt_aleatorio.place(relx=0.01, rely=0.1, relheight=0.10, relwidth=0.2)
 
-        self.bt_sequencial = Button(self.frame_2, text='reprodução \n sequencial', bd=0, bg='#FF8C00', fg='black', font=('verdana', 10, 'bold'))
+        self.bt_sequencial = Button(self.quadro_2, text='reprodução \n sequencial', bd=5, bg='#FF8C00', fg='black', font=('verdana', 10, 'bold'))
         self.bt_sequencial.place(relx=0.01, rely=0.3, relheight=0.10, relwidth=0.2)
 
-        self.label_mensagem = Label(self.frame_2, text='', font=('verdana', 12, 'bold'), bg='#1C1C1C', fg = '#FF8C00')
+        self.label_mensagem = Label(self.quadro_2, text='', font=('verdana', 12, 'bold'), bg='#1C1C1C', fg = '#FF8C00')
         self.label_mensagem.place(relx=0.4, rely=0.3)
 
-        self.label_mensagem_mes = Label(self.frame_2, text='', font = ('verdana', 12, 'bold' ), bg ='#1C1C1C', fg ='#FF8C00')
+        self.label_mensagem_mes = Label(self.quadro_2, text='', font = ('verdana', 12, 'bold' ), bg ='#1C1C1C', fg ='#FF8C00')
         self.label_mensagem_mes.place(relx = 0.7, rely = 0.3)
 
     def funcao_parar(self):
@@ -48,13 +48,13 @@ class Aplicacao:
 
     def combobox(self):
         self.valores_dias = list(range(1, 31))
-        self.combo_dias = ttk.Combobox(self.frame_1, values=self.valores_dias)
+        self.combo_dias = ttk.Combobox(self.quadro_1, values=self.valores_dias)
         self.combo_dias.set('Dia')
         self.combo_dias.place(relx=0.3, rely=0.2, relheight=0.4, relwidth=0.2)
         self.combo_dias.bind("<<ComboboxSelected>>", self.selecionar_dia)
 
         self.valores_mes = ['Janeiro', 'Fevereiro','março','abril','maio','junho','julho','agosto','setembro','outubro', 'novembro', 'dezembro']
-        self.combo_meses = ttk.Combobox(self.frame_1, values=self.valores_mes)
+        self.combo_meses = ttk.Combobox(self.quadro_1, values=self.valores_mes)
         self.combo_meses.set('Mês')
         self.combo_meses.place(relx=0.6, rely=0.2, relheight=0.4, relwidth=0.2)
         self.combo_meses.bind("<<ComboboxSelected>>", self.selecionar_mes)
